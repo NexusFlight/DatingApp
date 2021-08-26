a932d9358bec85d51f15c480fc7362d53a1dc0a6
Andrew Smirthwaite <a.smirthwaite@gmail.com>
2021-08-26 15:57:25 +0100

    Applied Fix

--- client/src/app/_services/message.service.ts
+++ client/src/app/_services/message.service.ts
@@ -4,9 +4,10 @@ import { HubConnection, HubConnectionBuilder } from '@microsoft/signalr';
 import { BehaviorSubject } from 'rxjs';
 import { take } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
-import { Group } from '../_models/Group';
+import { Group } from '../_models/group';
 import { Message } from '../_models/message';
 import { User } from '../_models/user';
+import { BusyService } from './busy.service';
 import { getPaginatedResult, getPaginationHeaders } from './paginationHelper';
 
 @Injectable({
@@ -19,17 +20,20 @@ export class MessageService {
   private messageThreadSource = new BehaviorSubject<Message[]>([]);
   messageThread$ = this.messageThreadSource.asObservable();
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private busyService: BusyService ) { }
 
   createHubConnection(user: User, otherUsername:string){
+    this.busyService.busy();
     this.hubConnection = new HubConnectionBuilder()
       .withUrl(this.hubUrl+"message?user="+otherUsername, {
         accessTokenFactory: () => user.token
       }).withAutomaticReconnect()
       .build()
 
-      this.hubConnection.start().catch(error => console.log(error));
-
+      this.hubConnection.start()
+        .catch(error => console.log(error))
+        .finally(() => this.busyService.idle());
+      
       this.hubConnection.on("ReceiveMessageThread", messages => {
         this.messageThreadSource.next(messages);
       });
@@ -56,6 +60,7 @@ export class MessageService {
 
   stopHubConnection(){
     if(this.hubConnection){
+      this.messageThreadSource.next([]);
       this.hubConnection.stop().catch(error => console.log(error));
     }
   }
